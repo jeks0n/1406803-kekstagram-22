8267274e96e52cb61bafc14f43b5ac879788c8ab
Evgenii Popov <jekson87@me.com>
2021-01-31 16:44:17 +0300

    Работа по заданию 2.10 Исправлние Eslint

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 const getInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
-const checkTextLength = (text, maxLength) =>  text.length <= maxLength;
+const checkTextLength = (text, maxLength) => text.length <= maxLength;
+
+getInteger(0, 10);
+checkTextLength('Test', 10);
