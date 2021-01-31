d5d7fcb806b88f007b188fcf915f2bfcc940804d
Evgenii Popov <jekson87@me.com>
2021-01-31 18:14:24 +0300

    Доработка по замечаниям к заданию

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-const getInteger = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+'use strict';
+
+const getInteger = (min, max) => {
+  if (min > max) {
+    throw new Error('Стартовое значение диапазона не может быть больше финального значения');
+  } else if (min < 0 || max < 0) {
+    throw new Error('Диапазон может содержать только положительные значения')
+  }
+
+  return Math.floor(min + Math.random() * (max + 1 - min))
+};
+
 const checkTextLength = (text, maxLength) => text.length <= maxLength;
 
 getInteger(0, 10);
