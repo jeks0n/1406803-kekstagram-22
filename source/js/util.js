7fa2a981b5c966db19cd3ad62a47905990e9853d
Evgenii Popov <jekson87@me.com>
2021-03-05 16:47:13 +0300

    Добавлена обработка режимов development/production

--- source/js/util.js
+++ source/js/util.js
@@ -1,31 +1,3 @@
-const getInteger = (min, max) => {
-  if (min > max) {
-    throw new Error('Стартовое значение диапазона не может быть больше финального значения');
-  } else if (min < 0 || max < 0) {
-    throw new Error('Диапазон может содержать только положительные значения')
-  }
-
-  return Math.floor(min + Math.random() * (max + 1 - min))
-};
-
-const getRandomArrayElement = (elements, { repeat = false } = {}) => {
-  if (repeat) {
-    return () => elements[getInteger(0, elements.length - 1)];
-  }
-
-  const copy = elements.slice().sort(() => Math.random() - 0.5);
-  let index = 0;
-
-  return () => {
-    if (index >= copy.length) {
-      throw new Error('Больше нет элементов для отображения');
-    }
-    index++;
-
-    return copy[index - 1];
-  }
-};
-
 const checkTextLength = (text, maxLength) => text.length <= maxLength;
 
 const isEscEvent = (evt) => {
@@ -49,4 +21,4 @@ const debounce = (cb, ms) => {
   };
 };
 
-export { getInteger, getRandomArrayElement, checkTextLength, isEscEvent, hasDuplicateValues, debounce };
+export { checkTextLength, isEscEvent, hasDuplicateValues, debounce };
